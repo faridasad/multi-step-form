eb71b27284208bc7788687ced565a6b6c1851b8e
faridasad <asadlifaridd@gmail.com>
2023-01-23 17:15:59 +0400

    Create fake data to use in forms

---
+++ src/db/FakeData.ts
@@ -0,0 +1,79 @@
+interface FakeAddOns {
+  name: string;
+  title: string;
+  description: string;
+  price: Price;
+}
+
+interface Price {
+  monthly: number;
+  yearly: number;
+}
+
+export const fakeAddOns: FakeAddOns[] = [
+  {
+    name: "online-service",
+    title: "Online Service",
+    description: "Access to multiplayer games",
+    price: {
+      monthly: 1,
+      yearly: 10,
+    },
+  },
+  {
+    name: "larger-storage",
+    title: "Larger storage",
+    description: "Increase storage capacity",
+    price: {
+      monthly: 2,
+      yearly: 20,
+    },
+  },
+  {
+    name: "customizable-profile",
+    title: "Customizable profile",
+    description: "Custom theme on your profile",
+    price: {
+      monthly: 3,
+      yearly: 30,
+    },
+  },
+];
+
+interface FakePlans {
+  name: string;
+  title: string;
+  price: Price;
+}
+
+interface Price {
+  monthly: number;
+  yearly: number;
+}
+
+export const fakePlans: FakePlans[] = [
+  {
+    name: "arcade",
+    title: "Arcade",
+    price: {
+      monthly: 9,
+      yearly: 90,
+    },
+  },
+  {
+    name: "advanced",
+    title: "Advanced",
+    price: {
+      monthly: 19,
+      yearly: 190,
+    },
+  },
+  {
+    name: "pro",
+    title: "Pro",
+    price: {
+      monthly: 29,
+      yearly: 290,
+    },
+  },
+];
